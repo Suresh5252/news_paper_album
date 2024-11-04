e4d109cfc26e43fc5a33a7928bb2f4590490feff
suresh5252 <sureshvasanth33@gmail.com>
2024-11-04 22:36:46 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'news_paper_album',
+  webDir: 'www'
+};
+
+export default config;
